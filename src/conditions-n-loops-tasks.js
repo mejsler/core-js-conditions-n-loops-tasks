7142f169adb0d8d2b7d1407e09ce9199e3a96f8f
mejsler <mejsler@pm.me>
2024-02-01 14:06:30 +0500

    feat: add solution for convertNumberToString

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -126,8 +126,33 @@ function convertToRomanNumerals(/* num */) {
  *  '10,5'    => 'one zero point five'
  *  '1950.2'  => 'one nine five zero point two'
  */
-function convertNumberToString(/* numberStr */) {
-  throw new Error('Not implemented');
+function convertNumberToString(numberStr) {
+  const kek = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    '.': 'point',
+    ',': 'point',
+    '-': 'minus',
+  };
+  let result = '';
+  for (let i = 0; i < numberStr.length; i += 1) {
+    switch (i) {
+      case numberStr.length - 1:
+        result += `${kek[numberStr[i]]}`;
+        break;
+      default:
+        result += `${kek[numberStr[i]]} `;
+    }
+  }
+  return result;
 }
 
 /**
